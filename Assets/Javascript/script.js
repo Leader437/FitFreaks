162fbe6b7d5e199d71585e2e69cfac776cca22c8
Saif <raosaif786pak@gmail.com>
2023-12-17 23:34:00 +0500

    added utility classes and created navbar

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,2 +1,37 @@
 import '../Css/normalize.css';
 import '../Css/style.css';
+
+
+let body = document.querySelector('body');
+let main = document.querySelector('main');
+let header = document.querySelector('header');
+let hamburger = document.querySelector('.hamburger');
+let stick1 = document.querySelector('.hamburger .one');
+let stick3 = document.querySelector('.hamburger .three');
+let navList = document.querySelector('.nav-list');
+let navItems = document.querySelectorAll('.nav-anchor');
+let scrollTop = document.querySelector('.scroll-top');
+
+
+
+
+
+
+
+
+hamburger.addEventListener('click', () => {
+    // Hamburger Animation
+    stick1.classList.toggle('one-active');
+    stick3.classList.toggle('three-active');
+    // Hamburger functionality
+    navList.classList.toggle('nav-active');
+});
+
+navItems.forEach(item => {
+    // Close navbar on click of any navLink in small screen devices
+    item.addEventListener('click', () => {
+        navList.classList.remove('nav-active');
+        stick1.classList.remove('one-active');
+        stick3.classList.remove('three-active');
+    });
+});
